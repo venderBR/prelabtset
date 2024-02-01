e3bac95a13bcffde33e97b45052d95b6f5603c09
Cxv_rbnoiu <142091560+venderBR@users.noreply.github.com>
2024-02-01 10:02:30 +0700

    ทำ validate 3 finish - close #4

--- backend/test/unit/User_test.go
+++ backend/test/unit/User_test.go
@@ -69,7 +69,7 @@ func TestStudentID(t *testing.T){
 		g.Expect(err).NotTo(BeNil())
 		g.Expect(err.Error()).To(Equal("StudentID is required"))
 	})
-	t.Run(`test_required` ,func(t *testing.T) {
+	t.Run(`test_invalid` ,func(t *testing.T) {
 
 		user := entity.User{
 			Firstname: "phit",
@@ -84,5 +84,42 @@ func TestStudentID(t *testing.T){
 		g.Expect(ok).NotTo(BeTrue())
 		g.Expect(err).NotTo(BeNil())
 		g.Expect(err.Error()).To(Equal("StudentID is invalid"))
+	})
+}
+
+func TestPhone(t *testing.T){
+	g := NewGomegaWithT(t)
+
+	t.Run(`test_required` ,func(t *testing.T) {
+
+		user := entity.User{
+			Firstname: "phit",
+			Lastname: "Ain",
+			StudentID: "b6404796",
+			Phone: "",
+			Email: "mangpor@gmail.com",
+		}
+
+		ok,err := govalidator.ValidateStruct(user)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(Equal("Phone is required"))
+	})
+	t.Run(`test_invalid` ,func(t *testing.T) {
+
+		user := entity.User{
+			Firstname: "phit",
+			Lastname: "Ain",
+			StudentID: "B6404796",
+			Phone: "06457233644",
+			Email: "mangpor@gmail.com",
+		}
+
+		ok,err := govalidator.ValidateStruct(user)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(Equal("Phone is invalid"))
 	})
 }
